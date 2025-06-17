6d605bf02965ce7e41b59962b361c2173099bfb7
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-06-17 22:42:27 +0530

    var,dyn and string

---
+++ ConsoleApp1/2VarAndDynamic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+class VarAndDynamic
+
+{
+     static void Main()
+    {
+        int value = 20;
+        value = 30;
+        Console.WriteLine(value);
+        Console.WriteLine();
+
+        string name = "bhaskar";
+        name = "c#.Net";
+        Console.WriteLine(name);
+        Console.WriteLine();
+
+        double prices = 121.2;
+        prices = 22.5;
+        Console.WriteLine(prices);
+        Console.WriteLine();
+
+        //     ------------ * Var * --------------- //
+
+
+        var number = 10;
+        Console.WriteLine("var number is : " + number);
+        Console.WriteLine("data type is : "+number.GetType());
+
+
+        var v_name = 10;
+        Console.WriteLine("v_name number is : " + v_name);
+        Console.WriteLine("data type is : " + v_name.GetType());
+Console.WriteLine();
+
+        //----------------  dynamic  ------------
+
+        dynamic d_number = 10;
+        d_number = 30;
+        Console.WriteLine("d_number is : " +d_number);
+        Console.WriteLine("d_number data type : "+d_number.GetType());
+        Console.WriteLine();
+
+        dynamic d_name= "C#.Net";
+        Console.WriteLine(d_name);
+        Console.WriteLine("d_name dtaatype : " + d_name.GetType());
+
+
+
+
+        d_name = "TCS";
+        Console.WriteLine(d_name.GetType());
+
+        Console.WriteLine($"v_name {d_name.Length}");
+
+        dynamic d_price = 12.5d;        
+        d_price = "Misard";
+        Console.WriteLine(d_price.GetType()); 
+
+
+
+
+    }
+
+
+}
---
+++ ConsoleApp1/3Strings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class String
+{
+    static void Main()
+    {
+        string strCourseName = "Csharp";  
+
+        string strCourseName1 = "\"Csharp\"";  
+        Console.WriteLine(strCourseName1);
+
+        string numberStrings = "One.\nTwo.\nThree"; 
+        Console.WriteLine(numberStrings);
+
+        string myProjectPath = @"D:\Csharp - Training\\CSharp\\CSharp_Step1\";
+        Console.WriteLine(myProjectPath);
+
+        
+        string name = "bhaskar";
+        int age = 23;
+        string message = $"My name is {name} and i am {age} yeals old";
+
+        Console.WriteLine(message);
+       
+    }
+
+}

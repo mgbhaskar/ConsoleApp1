8308bdc6bcf945a610711301828c8a247316acd9
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-06-20 00:36:34 +0530

    dt Conv.. Implicit,Explicit

---
+++ ConsoleApp1/6-dataconversation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class dataconversion
+{
+    static void Main()
+    {
+
+        //Implicit Conversions
+
+
+
+        //byte → short → int → long → float → double
+
+        //          char → int
+
+        //         int → float
+
+        //        float → double
+
+
+        byte smallNumber = 255;
+        int largeNumber = smallNumber; 
+        Console.WriteLine(largeNumber);
+        Console.WriteLine();
+
+
+        //
+
+        int NoOfEmployees = 1000;     // casting is data
+        byte ConvertingNoOfEmployees = (byte)NoOfEmployees;
+        Console.WriteLine(ConvertingNoOfEmployees);   //  o/p= 144 ==> 1000 - 256 =744-256 =488-256=232
+
+
+    }
+
+}

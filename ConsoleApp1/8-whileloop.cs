9593fb3bf1ca7fa9aecea03fe1bb5bfa09adbba1
bhaskar <mgbhaskarreddy.fullstack@gmail.com>
2025-06-24 21:56:32 +0530

    whileloop

---
+++ ConsoleApp1/8-whileloop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+
+class whileloop
+{
+    static void Main()
+    {
+        //    int count = 0;
+        //    while (count <5)
+        //    {
+        //        Console.WriteLine("statement executed...");
+        //        count = count + 1;
+        //}
+        //}
+        int myindiancurrency = 0;
+
+        Console.WriteLine($"my currency is before while loop: {myindiancurrency }");
+
+        while (myindiancurrency < 5) 
+        {
+            //myindiancurrency = myindiancurrency + 1;
+            Console.WriteLine(myindiancurrency);
+            myindiancurrency++;
+
+        }
+        int[] currency = { 50, 100, 100, 200, 300 };
+        int countingstage = 0;
+        int totalamount = 0;
+        while(countingstage < currency.Length) {
+            Console.Write(currency[countingstage]);
+    
+}
+    }
